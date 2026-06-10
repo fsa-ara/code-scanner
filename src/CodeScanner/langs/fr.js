a8c5a35ba5c0291cadda68b46ce72cd1bd46815d
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-06-10 17:53:06 +0200

    feat(CodeScanner): add langs translations

---
+++ src/CodeScanner/langs/fr.js
@@ -0,0 +1,8 @@
+export const FR = {
+    description: 'Placer le code-barres dans la zone de détection.',
+    alert: {
+        detected: 'Analyse en cours...',
+        undetected: 'Code-barres indétectable !',
+    },
+    button: 'Retour',
+};
---
+++ src/CodeScanner/langs/translate.js
@@ -0,0 +1,7 @@
+import { EN } from './en';
+import { FR } from './fr';
+
+export const TRANSLATES = {
+    en: EN,
+    fr: FR,
+};
